3516427a53bdef2bcd80f19950861643fc725485
Shashank23 <shashankmahaseth2323@gmail.com>
2025-11-03 22:28:17 +0530

    StylishApp

--- app/src/main/java/com/example/stylishe_commerceapp/presentation/HomePage/HomeScreen.kt
+++ app/src/main/java/com/example/stylishe_commerceapp/presentation/HomePage/HomeScreen.kt
@@ -1,8 +1,12 @@
 package com.example.stylishe_commerceapp.presentation.HomePage
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun HomeScreen(){
+Text(
+    text="Welcome"
 
+)
 }
